7109ef0d8e1e84b0ff1d6e1fcb2d8699de702a92
Andrés Miño <andresminio@gmail.com>
2022-01-27 11:02:32 -0300

    Add files via upload

---
+++ CODIGO redes partidarias 2011 2021.R
@@ -0,0 +1,26 @@
+#Alianzas Argentina 2011-2021  
+
+*instalar packages 
+install.packages("arm") 
+install.packages("ergm")
+install.packages("statnet")
+install.packages("foreign")
+install.packages("igraph")
+
+T <- Data1[,7:48]
+T<- as.matrix(T)
+Tt<-t(T)%*%T
+
+# Abrimos un objeto Tiff para guardar el grafico
+net<-graph.adjacency(Tt,mode="undirected",weighted=TRUE,diag=FALSE) 
+summary(net)
+
+#Agrega el "count" de colaboracion para ponderar cuan frecuentemente cooperan
+E(net)$weight
+V(net)
+tiff(filename = "alianzas15.tiff", width = 8, height = 8, units = "in", pointsize = 14, compression = c("none"), bg = "white", res =800)
+plot.igraph(net,vertex.label=V(net)$name, layout=layout.fruchterman.reingold, vertex.color=factor(V(net)), vertex.size=log(diag(Tt)), edge.width=E(net)$weight/4, vertex.label.cex=log(diag(Tt))/5, edge.curved=TRUE, edge.lty=3, edge.width=.3, edge.color=gray(.8), vertex.label.dist=rnorm(length(V(net)),.2,.03))
+dev.off()
+
+#guarda la matriz de datos
+write.csv(Tt, file="matriz15.csv", row.names=F) 
